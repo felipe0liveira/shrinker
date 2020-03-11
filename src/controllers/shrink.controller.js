26bbaf357b143df3d4367ec018a9d5ef8c0db4a2
Felipe Oliveira <felpd3v@gmail.com>
2020-03-11 15:41:42 -0300

    - Delete removed

--- src/controllers/shrink.controller.js
+++ src/controllers/shrink.controller.js
@@ -6,18 +6,6 @@ console.log('\n# Mounting Shrink Controller');
 
 router.use(bodyParser.json());
 
-router.get('/:shortUrl', async (req, res) => {
-    const shrinked = await shrinkSchema.findOne({ short: req.params.shortUrl });
-    if (shrinked) {
-        shrinkSchema.findByIdAndUpdate(shrinked._id, { clicks: (shrinked.clicks + 1) }, (err, value) => {
-            if (err) return res.status(500).json(err);
-            res.redirect(shrinked.url);
-        });
-    } else {
-        res.status(404).json({ message: 'Short URL not found' });
-    }
-});
-
 router.post('/', (req, res) => {
     shrinkSchema.create(req.body, (err, shrinked) => {
         if (err) return res.status(500).json(err);
@@ -28,6 +16,18 @@ router.post('/', (req, res) => {
 router.get('/', async (req, res) => {
     const list = await shrinkSchema.find();
     res.status(200).json(list);
+});
+
+router.get('/:short', async (req, res) => {
+    const shrinked = await shrinkSchema.findOne({ short: req.params.short });
+    if (shrinked) {
+        shrinkSchema.findByIdAndUpdate(shrinked._id, { clicks: (shrinked.clicks + 1) }, (err, value) => {
+            if (err) return res.status(500).json(err);
+            res.redirect(shrinked.url);
+        });
+    } else {
+        res.status(404).json({ message: 'Short URL not found' });
+    }
 });
 
 module.exports = router;
